5b4931b3833d876b0f262c99955fefff2602638e
Antoine Ceol <antoine.ceol@gmail.com>
2024-12-21 00:12:01 +0100

    day 2 step 1

--- src/advent.kt
+++ src/advent.kt
@@ -53,3 +53,34 @@ fun day1Step2() {
     val methodName = object {}.javaClass.enclosingMethod.name
     println("Advent of code $methodName, I found an answer: $index")
 }
+
+fun isValid(reports: List<Int>): Boolean{
+    val diff = mutableListOf<Int>()
+    var previous = -1;
+    for (index in reports.indices) {
+        val level = reports[index]
+        if (previous != -1)
+            diff.add(level - previous)
+        previous = level;
+    }
+
+    //print("$it ::")
+    diff.sort();
+    val increase =
+        (diff.first() > 0 && diff.last() < 4) || (diff.first() > -4 && diff.last() < 0)
+    val isDecreasingOrIncreasing = (diff.first() * diff.last()) >= 0
+    //println("diff :: $increase $isDecreasingOrIncreasing")
+    return increase && isDecreasingOrIncreasing
+}
+
+
+//day 2
+// 379
+fun day021() {
+    val data = readFileAsLinesUsingUseLines("./src/day2.txt");
+
+    val safeCount = data.map { isValid(it) }.count {it}
+
+    val methodName = object {}.javaClass.enclosingMethod.name
+    println("Advent of code $methodName, I found an answer: $safeCount")
+}
