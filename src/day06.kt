1249033ae87f45fbef34c2699753350c9bc17e56
Antoine Ceol <antoine.ceol@gmail.com>
2025-01-02 23:25:30 +0100

    day 6 step 2 iterative

--- src/day06.kt
+++ src/day06.kt
@@ -3,10 +3,28 @@ class Position(var x: Int, var y: Int){
         return "x: $x, y: $y"
     }
 
+    override fun hashCode(): Int {
+        return (x+1)*10000 + (y+1)*100
+    }
+
     fun move(move: Moves): Position{
         return Position(x+ move.x, y + move.y)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Position
+
+        if (x != other.x) return false
+        if (y != other.y) return false
+
+        return true
+    }
 }
+
+
 
 enum class Moves (val x: Int, val y: Int) {
     UP(0, -1),
@@ -47,6 +65,44 @@ fun day061() {
     println("Advent of code $methodName, I found an answer: $xs")
 }
 
+fun day062() {
+    val map = readArrayOfChars("./src/day061.txt")
+    var loop = 0
+
+    map.forEachIndexed { y, line -> line.forEachIndexed lineForEach@ {
+        x, column ->
+        var position = initial(map)
+        var direction = Moves.UP
+        var history = HashMap<Position, MutableList<Moves>>()
+        val updatedMap = readArrayOfChars("./src/day061.txt")
+        if(updatedMap[y][x] == '#' && updatedMap[y][x] == '^'){
+            return@lineForEach
+        }
+        updatedMap[y][x] = '#'
+
+        while(!isOut(updatedMap, position)) {
+            if(isLoop(position, direction, history)){
+                loop ++
+                return@lineForEach
+            }
+            var directions = history.get(position)
+            if(directions == null) history.put(position, mutableListOf(direction))
+            else directions.add(direction)
+
+            val newPosition = position.move(direction)
+            if (!isOut(updatedMap, newPosition) && updatedMap[newPosition.y][newPosition.x] == '#') {
+                direction = direction.moveRight()
+            } else {
+                position = newPosition
+            }
+    } }
+
+    }
+
+    val methodName = object {}.javaClass.enclosingMethod.name
+    println("Advent of code $methodName, I found an answer: $loop")
+}
+
 fun initial(map: List<List<Char>>): Position{
     val character = '^'
     val y = map.indexOfFirst { it.any {it === character} }
@@ -77,4 +133,7 @@ fun countX(map: List<List<Char>>): Int{
     return map.foldIndexed(0) { index, acc, next ->
         acc + map[index].count{it == 'X'}
     }
+}
+fun isLoop(currentPosition: Position, currentMove: Moves, past: Map<Position, List<Moves>>): Boolean {
+    return past.get(currentPosition)?.contains(currentMove) ?: false
 }
