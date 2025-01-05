19bb97093c5d209ef53ff8b6da6316e48996dde2
Antoine Ceol <antoine.ceol@gmail.com>
2025-01-05 22:55:58 +0100

    day 7 step 2

--- src/day07.kt
+++ src/day07.kt
@@ -1,9 +1,8 @@
-fun day071() {
-    val map
-     = readArrayOfStrings("./src/day07.txt", ":").associateBy(
-        {it.get(0).toLong()}, {it.get(1).split(" ").filter{it -> it.isNotEmpty()}.map(String::toLong)}
-     ).toMap()
+val map = readArrayOfStrings("./src/day07.txt", ":").associateBy(
+    {it.get(0).toLong()}, {it.get(1).split(" ").filter{it -> it.isNotEmpty()}.map(String::toLong)}
+).toMap()
 
+fun day071() {
     val res = map.entries.fold(0.toLong()) {acc, entry ->
         if(compose(entry.value).contains(entry.key))
             acc + entry.key
@@ -15,24 +14,39 @@ fun day071() {
     println("Advent of code $methodName, I found an answer: $res")
 }
 
+fun day072() {
+    val res = map.entries.fold(0.toLong()) {acc, entry ->
+        if(compose(entry.value, 2).contains(entry.key))
+            acc + entry.key
+        else
+            acc
+    }
+
+    val methodName = object {}.javaClass.enclosingMethod.name
+    println("Advent of code $methodName, I found an answer: $res")
+}
+
 fun multiply (left:Long, right: Long): Long { return left * right}
 fun add (left:Long, right: Long): Long { return left + right}
-val operations: List<(Long, Long) -> Long> = listOf(::multiply, ::add)
+fun concat (left:Long, right: Long): Long { return (left.toString() + right.toString()).toLong()}
+val operations: List<(Long, Long) -> Long> = listOf(::multiply, ::add, :: concat)
 
 
-fun addOperations(currentResult: Long, values: List<Long>, results: MutableList<Long>, index: Int){
+fun addOperations(currentResult: Long, values: List<Long>, results: MutableList<Long>, index: Int, maxOp: Int){
     if(values.isEmpty()){
         results.add(currentResult)
     } else {
         val newResult = operations[index](currentResult, values.first())
-        addOperations(newResult, values.drop(1), results, 1)
-        addOperations(newResult, values.drop(1), results, 0)
+        for(i in 0..maxOp) {
+            addOperations(newResult, values.drop(1), results, i, maxOp)
+        }
     }
 }
 
-fun compose(values: List<Long>): List<Long> {
+fun compose(values: List<Long>, maxOp: Int = 1): List<Long> {
     val results = mutableListOf<Long>()
-    addOperations(values.first(), values.drop(1), results, 0)
-    addOperations(values.first(), values.drop(1), results, 1)
+    for(i in 0..maxOp) {
+        addOperations(values.first(), values.drop(1), results, i, maxOp)
+    }
     return results
 }
