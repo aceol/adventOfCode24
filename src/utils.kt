f5f527cef0f6ca2fe319d36ea7a1ac4372ba1096
Antoine Ceol <antoine.ceol@gmail.com>
2025-01-06 22:54:38 +0100

    refacto day06

--- src/utils.kt
+++ src/utils.kt
@@ -34,3 +34,34 @@ fun readOrders(fileName: String, delimiter: String = ""): List<List<Int>>
 
 fun readTextFile(fileName: String): String
         = File(fileName).readText()
+
+
+class Position(var x: Int, var y: Int){
+    override fun toString(): String {
+        return "x: $x, y: $y"
+    }
+
+    override fun hashCode(): Int {
+        return (x+1)*10000 + (y+1)*100
+    }
+
+    fun move(move: Moves): Position{
+        return Position(x+ move.x, y + move.y)
+    }
+
+    fun isOut(map: List<List<Char>>): Boolean{
+        return x< 0 || y< 0 || y >= map.size || x >= map.first().size
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Position
+
+        if (x != other.x) return false
+        if (y != other.y) return false
+
+        return true
+    }
+}
